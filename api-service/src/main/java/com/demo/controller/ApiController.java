017b0184215bd3bb69214ddb4b385194ae025265
Prithvi Sharma <53996107+prithvisharma@users.noreply.github.com>
2022-09-29 01:16:44 +0530

    update(controller): asign path value to controller

--- api-service/src/main/java/com/demo/controller/ApiController.java
+++ api-service/src/main/java/com/demo/controller/ApiController.java
@@ -5,11 +5,13 @@ import com.google.protobuf.Descriptors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping(value = "api")
 public class ApiController {
 
     @Autowired
