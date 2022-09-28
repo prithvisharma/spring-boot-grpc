c042820517dad90bbf96ab9eabf66ccc4d1a00bd
Prithvi Sharma <53996107+prithvisharma@users.noreply.github.com>
2022-09-29 00:44:54 +0530

    update(database): update wrt change in User schema

--- user-service/src/main/java/com/demo/database/TempDb.java
+++ user-service/src/main/java/com/demo/database/TempDb.java
@@ -9,10 +9,10 @@ public class TempDb {
     public static List<User> getUsersFromTempDb() {
         return new ArrayList<User>() {
             {
-                add(User.newBuilder().setId("U101").setName("Prithvi Sharma").setAge(23).setGender("MALE").setPhone(9096410286d)
+                add(User.newBuilder().setId("U101").setName("Prithvi Sharma").setAge(23).setGender("MALE").setPhone(9096410286l)
                         .setEmail("prithvi2255@gmail.com").setAddress("Nalanda, Naigaon East").setCity("Mumbai").setState("Maharashtra")
                         .setPincode(401208).build());
-                add(User.newBuilder().setId("U102").setName("John Doe").setAge(33).setGender("MALE").setPhone(92232954532d)
+                add(User.newBuilder().setId("U102").setName("John Doe").setAge(33).setGender("MALE").setPhone(92232954532l)
                         .setEmail("johndoe@gmail.com").setAddress("Gokuldham, Goregaon East").setCity("Mumbai").setState("Maharashtra")
                         .setPincode(400063).build());
             }
