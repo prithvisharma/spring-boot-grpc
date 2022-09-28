cac8796f22cadcf8e6e5bc724dab7c9e52fddc40
Prithvi Sharma <53996107+prithvisharma@users.noreply.github.com>
2022-09-29 02:40:59 +0530

    update(service): implement getAlikeUsers()

--- api-service/src/main/java/com/demo/service/ApiService.java
+++ api-service/src/main/java/com/demo/service/ApiService.java
@@ -3,10 +3,16 @@ package com.demo.service;
 import com.demo.User;
 import com.demo.UserServiceGrpc;
 import com.google.protobuf.Descriptors;
+import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ApiService {
@@ -14,9 +20,37 @@ public class ApiService {
     @GrpcClient("grpc-client")
     private UserServiceGrpc.UserServiceBlockingStub userGrpcClient;
 
+    @GrpcClient("grpc-client")
+    private UserServiceGrpc.UserServiceStub userGrpcStreamingClient;
+
     public Map<Descriptors.FieldDescriptor, Object> getUser(String id){
         final User user = User.newBuilder().setId(id).build();
         final User grpcResponse = userGrpcClient.getUser(user);
         return grpcResponse.getAllFields();
+    }
+
+    public List<Map<Descriptors.FieldDescriptor, Object>> getAlikeUsers(String nameLike) throws InterruptedException {
+        final User user = User.newBuilder().setName(nameLike).build();
+        final List<Map<Descriptors.FieldDescriptor, Object>> response = new ArrayList<>();
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        userGrpcStreamingClient.getAlikeUser(user, new StreamObserver<User>() {
+            @Override
+            public void onNext(User value) {
+                response.add(value.getAllFields());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                countDownLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                countDownLatch.countDown();
+            }
+        });
+
+        boolean await = countDownLatch.await(1, TimeUnit.MINUTES);
+        return await ? response : Collections.emptyList();
     }
 }
