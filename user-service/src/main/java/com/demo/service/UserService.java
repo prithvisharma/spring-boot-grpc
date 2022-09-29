3e1a0a50cc13ddc98179d65182194ca4b543c869
Prithvi Sharma <53996107+prithvisharma@users.noreply.github.com>
2022-09-29 12:04:35 +0530

    update(service): implement updateUsers() autowire UserServiceHandler

--- user-service/src/main/java/com/demo/service/UserService.java
+++ user-service/src/main/java/com/demo/service/UserService.java
@@ -5,6 +5,7 @@ import com.demo.DeleteResponse;
 import com.demo.User;
 import com.demo.UserServiceGrpc;
 import com.demo.database.UserDatabase;
+import com.demo.handler.UserServiceHandler;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,10 @@ import java.util.List;
 public class UserService extends UserServiceGrpc.UserServiceImplBase {
     @Autowired
     private UserDatabase userDatabase;
+
+    @Autowired
+    private UserServiceHandler userServiceHandler;
+
     @Override
     public void getUser(User request, StreamObserver<User> responseObserver) {
         final User user = userDatabase.findById(request.getId());
@@ -39,8 +44,6 @@ public class UserService extends UserServiceGrpc.UserServiceImplBase {
             public void onNext(User user) {
                 final String id = userDatabase.insert(user);
                 final User finalUser = User.newBuilder(user).setId(id).build();
-                System.out.println(finalUser.getId());
-                System.out.println(finalUser.getName());
                 responseObserver.onNext(finalUser);
             }
 
@@ -59,7 +62,36 @@ public class UserService extends UserServiceGrpc.UserServiceImplBase {
 
     @Override
     public StreamObserver<User> updateUsers(StreamObserver<User> responseObserver) {
-        return super.updateUsers(responseObserver);
+        return new StreamObserver<User>() {
+            final List<User> userList = new ArrayList<>();
+
+            @Override
+            public void onNext(User newUser) {
+                System.out.println(newUser.getAllFields());
+                final User dbUser = userDatabase.findById(newUser.getId());
+                System.out.println(dbUser.getAllFields());
+                final User finalUser = userServiceHandler.handleUpdateUser(newUser, dbUser);
+                System.out.println(finalUser.getAllFields());
+//                final boolean isUpdated = userDatabase.updateById(finalUser);
+                if(true){
+                    responseObserver.onNext(finalUser);
+                }
+                else {
+                    responseObserver.onNext(dbUser);
+                }
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                responseObserver.onError(throwable);
+            }
+
+            @Override
+            public void onCompleted() {
+                userList.forEach(responseObserver::onNext);
+                responseObserver.onCompleted();
+            }
+        };
     }
 
     @Override
